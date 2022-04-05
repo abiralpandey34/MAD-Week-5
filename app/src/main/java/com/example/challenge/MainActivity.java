a34e290340e5b8ec0a1c4e73bc612203abf6774f
Abiral <abiral.pandey34@gmail.com>
2022-04-05 09:56:53 +0545

    Homework Complete

--- app/src/main/java/com/example/challenge/MainActivity.java
+++ app/src/main/java/com/example/challenge/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.ContextThemeWrapper;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,53 +19,35 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    List<String> itemsList = new ArrayList<>();
-    LinearLayout listContainer;
-    FloatingActionButton addButton;
-    public static final String EXTRA_MESSAGE = "com.example.twoactivity.extra.REPLY";
-    public static final int MAIN_ACTIVITY_REQUEST= 1;
+    int count;
+    Button countButton;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addButton = findViewById(R.id.floatingActionButton);
-
         setContentView(R.layout.activity_main);
 
-        listContainer = findViewById(R.id.list_container);
-        this.renderList(this.itemsList);
+        if(savedInstanceState!=null){
+            this.count = savedInstanceState.getInt("count");
+        }else{
+            this.count = 0;
+        }
+
+        countButton = findViewById(R.id.button);
+        text = findViewById(R.id.textView);
+
+        text.setText(count+"");
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == MAIN_ACTIVITY_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                String reply = data.getStringExtra(com.example.challenge.SecondActivity.EXTRA_REPLY);
-
-                this.itemsList.add(reply);
-                this.renderList(this.itemsList);
-            }
-        }
+    protected void onSaveInstanceState(Bundle bundle){
+        super.onSaveInstanceState(bundle);
+        bundle.putInt("count", count);
     }
 
-    public void addItem(View view) {
-        Intent intent = new Intent(this, com.example.challenge.SecondActivity.class);
-        intent.putExtra(EXTRA_MESSAGE, TextUtils.join(",", this.itemsList));
-        startActivityForResult(intent, MAIN_ACTIVITY_REQUEST);
+    public void countUp(View view) {
+        this.count++;
+        this.text.setText(count+"");
     }
-
-
-    private void renderList(List<String> shoppingItems){
-        this.listContainer.removeAllViews();
-
-        for (int i = 0; i < shoppingItems.size(); i++) {
-            TextView listItem = new TextView(new ContextThemeWrapper(MainActivity.this, R.style.list_style));
-            listItem.setText(shoppingItems.get(i));
-
-            this.listContainer.addView(listItem);
-        }
-    }
-
 }
